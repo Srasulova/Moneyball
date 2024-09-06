7be1770b0f1a001713d88296d50f4b841b9c5b1a
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-09-06 02:07:09 -0400

    WIP add initial routes for user and auth

---
+++ backend/routes/users.js
@@ -0,0 +1,72 @@
+"use strict";
+
+/** Routes for users. */
+
+const jsonschema = require("jsonschema");
+
+const express = require("express");
+const { ensureCorrectUser } = require("../middleware/auth");
+const { BadRequestError } = require("../expressError");
+const User = require("../models/user");
+const { createToken } = require("../helpers/tokens");
+const userNewSchema = require("../schemas/userNew.json");
+const userUpdateSchema = require("../schemas/userUpdate.json");
+
+const router = express.Router();
+
+router.get("/", ensureCorrectUser, async function (req, res, next) {
+  try {
+    const validator = jsonschema.validate(req.body, userNewSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map((e) => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const user = await User.register(req.body);
+    const token = createToken(user);
+    return res.status(201).json({ user, token });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** PATCH /[username] { user } => { user }
+ *
+ * Data can include:
+ *   { firstName, password, email }
+ *
+ * Returns { firstName, email }
+ *
+ * Authorization required: same-user-as-:email
+ **/
+
+router.patch("/:firstName", ensureCorrectUser, async function (req, res, next) {
+  try {
+    const validator = jsonschema.validate(req.body, userUpdateSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map((e) => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const user = await User.update(req.params.firstName, req.body);
+    return res.json({ user });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** DELETE /[username]  =>  { deleted: firstName }
+ *
+ * Authorization required: same-user-as-:email
+ **/
+
+router.delete("/:username", ensureCorrectUser, async function (req, res, next) {
+  try {
+    await User.remove(req.params.username);
+    return res.json({ deleted: req.params.username });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
