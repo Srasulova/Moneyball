636af070b0cac6033d010ed148f75d541a542eeb
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-09-09 16:15:44 -0400

    write routes in favorites.js

---
+++ backend/routes/favorites.js
@@ -0,0 +1,139 @@
+"use strict";
+
+/** Routes for favorite teams and players. */
+
+const express = require("express");
+const { ensureLoggedIn } = require("../middleware/auth");
+const User = require("../models/user");
+
+const router = express.Router();
+
+/** GET /favorites/teams  => { favoriteTeams }
+ *
+ * Returns the current user's favorite teams.
+ *
+ * Authorization required: logged-in
+ **/
+router.get("/favorites/teams", ensureLoggedIn, async function (req, res, next) {
+  try {
+    const favoriteTeams = await User.getFavoriteTeams(res.locals.user.email);
+    return res.json({ favoriteTeams });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** GET /favorites/players  => { favoritePlayers }
+ *
+ * Returns the current user's favorite players.
+ *
+ * Authorization required: logged-in
+ **/
+router.get(
+  "/favorites/players",
+  ensureLoggedIn,
+  async function (req, res, next) {
+    try {
+      const favoritePlayers = await User.getFavoritePlayers(
+        res.locals.user.email
+      );
+      return res.json({ favoritePlayers });
+    } catch (err) {
+      return next(err);
+    }
+  }
+);
+
+/** POST /favorites/teams/:teamId  => { favoriteTeams }
+ *
+ * Adds a team to the current user's favorites.
+ *
+ * Authorization required: logged-in
+ **/
+router.post(
+  "/favorites/teams/:teamId",
+  ensureLoggedIn,
+  async function (req, res, next) {
+    try {
+      const { teamId } = req.params;
+      const favoriteTeams = await User.addFavoriteTeam(
+        res.locals.user.email,
+        teamId
+      );
+      return res.json({ favoriteTeams });
+    } catch (err) {
+      return next(err);
+    }
+  }
+);
+
+/** DELETE /favorites/teams/:teamId  => { favoriteTeams }
+ *
+ * Removes a team from the current user's favorites.
+ *
+ * Authorization required: logged-in
+ **/
+router.delete(
+  "/favorites/teams/:teamId",
+  ensureLoggedIn,
+  async function (req, res, next) {
+    try {
+      const { teamId } = req.params;
+      const favoriteTeams = await User.removeFavoriteTeam(
+        res.locals.user.email,
+        teamId
+      );
+      return res.json({ favoriteTeams });
+    } catch (err) {
+      return next(err);
+    }
+  }
+);
+
+/** POST /favorites/players/:playerId  => { favoritePlayers }
+ *
+ * Adds a player to the current user's favorites.
+ *
+ * Authorization required: logged-in
+ **/
+router.post(
+  "/favorites/players/:playerId",
+  ensureLoggedIn,
+  async function (req, res, next) {
+    try {
+      const { playerId } = req.params;
+      const favoritePlayers = await User.addFavoritePlayer(
+        res.locals.user.email,
+        playerId
+      );
+      return res.json({ favoritePlayers });
+    } catch (err) {
+      return next(err);
+    }
+  }
+);
+
+/** DELETE /favorites/players/:playerId  => { favoritePlayers }
+ *
+ * Removes a player from the current user's favorites.
+ *
+ * Authorization required: logged-in
+ **/
+router.delete(
+  "/favorites/players/:playerId",
+  ensureLoggedIn,
+  async function (req, res, next) {
+    try {
+      const { playerId } = req.params;
+      const favoritePlayers = await User.removeFavoritePlayer(
+        res.locals.user.email,
+        playerId
+      );
+      return res.json({ favoritePlayers });
+    } catch (err) {
+      return next(err);
+    }
+  }
+);
+
+module.exports = router;
