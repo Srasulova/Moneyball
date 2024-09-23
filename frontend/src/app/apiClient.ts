52080dc6576a4a24d1c268b4b2ee6a3a4cce1654
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-09-23 19:52:52 -0400

    add public url to the env file

--- frontend/src/app/apiClient.ts
+++ frontend/src/app/apiClient.ts
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
 
 class User {
   // Helper method for making requests
