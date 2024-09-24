ecb96153515b6fbffe79515fb4f7151f709ff1f1
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-09-24 01:28:28 -0400

    wip

--- frontend/src/app/appContext.tsx
+++ frontend/src/app/appContext.tsx
@@ -2,15 +2,27 @@
 
 import { createContext, useEffect, useState } from "react";
 import Navbar from "./UI/Navbar";
+import { useRouter } from "next/router";
+import { redirect } from "next/navigation";
 
 export const AuthContext = createContext<boolean>(false);
 
 export default function AppContext({ children }: { children: React.ReactNode }) {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const router = useRouter();
 
     useEffect(() => {
-        localStorage.getItem("token") ? setIsLoggedIn(true) : setIsLoggedIn(false);
-    }, []);
+        let token = localStorage.getItem("token");
+        if (token) {
+            setIsLoggedIn(true)
+        } else {
+            setIsLoggedIn(false);
+
+            if (router.pathname !== '/login' && router.pathname !== '/signup') {
+                redirect('/login');
+            }
+        }
+    }, [router]);
 
     return (
         <AuthContext.Provider value={isLoggedIn}>
