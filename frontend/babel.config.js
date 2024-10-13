8f64e779f51ca6956aabd17f06235267161c63e5
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-10-13 01:13:21 -0400

    fix build issue

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,7 +1,38 @@
-module.exports = {
-  presets: [
-    "@babel/preset-env",
-    "@babel/preset-react",
-    "@babel/preset-typescript",
-  ],
+// module.exports = {
+//   presets: [
+//     "@babel/preset-env",
+//     "@babel/preset-react",
+//     "@babel/preset-typescript",
+//   ],
+// };
+
+// module.exports = {
+//   presets: ["next/babel"],
+// };
+
+// module.exports = (api) => {
+//   api.cache(true);
+
+//   const isTest = process.env.NODE_ENV === "test";
+
+//   const presets = isTest
+//     ? ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"]
+//     : ["next/babel"]; // Use next/babel preset for production
+
+//   return { presets };
+// };
+
+const path = require("path");
+
+module.exports = (api) => {
+  api.cache(true);
+
+  const isTest = process.env.NODE_ENV === "test";
+
+  // Use different configurations based on the environment
+  return isTest
+    ? require(path.resolve(__dirname, "babel.test.config.js"))
+    : {
+        presets: ["next/babel"],
+      };
 };
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./UI/Navbar";
 import Footer from "./UI/Footer";
 import AppContext, { AuthContext } from "./appContext";
 import { useContext } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Moneyball",
@@ -21,7 +21,8 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body className={inter.className}>
+      {/* <body className={inter.className}> */}
+      <body >
         <AppContext>
           <main className="flex-1">
             {children}
